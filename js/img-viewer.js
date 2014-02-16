890b42c5d31666bb8ff6dd0cf726a9d5cf44a748
dvovna <d.vovna.dv@gmail.com>
2014-02-16 23:54:08 +0400

    structure creating;

---
+++ js/img-viewer.js
@@ -0,0 +1,12 @@
+window.IV = {};
+
+(function () {
+    'use strict';
+
+    window.ImageViewer = Backbone.Router.extend({
+        initialize: function (options) {
+            this.options = options || {};
+            console.log('start');
+        }
+    })
+}());
